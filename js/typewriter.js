3e7af9518576713ef8e6ac6bfdd4856a1e90fbe4
Kim Gray <kim.lc.gray@gmail.com>
2019-07-26 19:32:25 +0100

    north page changes

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,19 +1,13 @@
-var typeString = ["I'm Wildo, a travel blogger and photographer. Take a look around my blog to discover a treasure trove of hidden London gems."];
 var i = 0;
-var count = 0;
-var selectedText = "";
+var selectedText = "I'm Wildo, a travel blogger and photographer. Take a look around my blog to discover a treasure trove of hidden London gems.";
 var text = "";
 
 (function type() {
-	if (count == typeString.length) {
-		count = 0;
-	}
-	selectedText = typeString[count];
 	text = selectedText.slice(0, ++i);
 	document.getElementById("typing").innerHTML = text;
-	if (text.length === selectedText.length) {
-		count++;
-		i = 0;
-	}
+	// Comment this back in to reset typing after it's finished
+	// if (text.length === selectedText.length) {
+	// 	i = 0;
+	// }
 	setTimeout(type, 200);
 })();
